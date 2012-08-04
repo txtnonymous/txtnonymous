ab5ba1eb5d8c140e4b6d556173f2c05724d6433e
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-08-04 17:32:21 +0100

    Import db.{find,find_or_create}

--- db.py
+++ db.py
@@ -1,16 +1,36 @@
 """Database module"""
 
+from exceptions import RuntimeError
+from datetime import datetime, timedelta
 from pymongo import Connection
 connection = Connection()
 
 # Get your DB
-db = connection['default']
+db = connection['txtnonymous-dev']
+
+class NotFoundException(RuntimeError):
+    """Database record not found"""
+
+def create_user(gid):
+    oid = db.users.insert({
+        'gid': gid,
+        'tag': None,
+        'expires': datetime.now()+timedelta(days=1)})
+    print oid
+    # FIXME: use the first 7 characters from the uid as the tag
+    tag = str(oid)[:8]
+    db.test.update({"_id": oid}, {"$set": {"tag": tag}})
+    return db.users.find_one(oid)
 
 def find_or_create(gid):
-    return None
+    u = db.users.find_one({"gid": gid})
+    return u or create_user(gid)
 
 def find(gid):
-    return None
+    u = db.users.find_one({"gid": gid})
+    if not u:
+        raise
+    return u
 
 def extend_timestamp(user):
     pass
