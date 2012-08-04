3e05d1be47ef2b6eab3b423a8610b14cf77ef7c8
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-08-04 15:57:14 +0100

    Read twilio config from env

--- sms.py
+++ sms.py
@@ -1,9 +1,11 @@
+import os
 from twilio.rest import TwilioRestClient
 import credentials
 
 class Twilio:
     def __init__(self):
-        self.client = TwilioRestClient(credentials.sid, credentials.auth)
+        self.client = TwilioRestClient(os.environ.get('TWILIO_SID', credentials.sid),
+                os.environ.get('TWILIO_AUTH', credentials.auth))
 
     def send_sms(self, number, message):
         message = self.client.sms.messages.create(to = number, from_ = credentials.phone_number,
