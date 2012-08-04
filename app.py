1c69764d506ed2e6a5e0349210bfe3f83f08f696
Joe Jordan <joe.jordan86@gmail.com>
2012-08-04 17:17:38 +0100

    adding interactive features

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os, re, db
 from flask import Flask
 from flask import request
-
 import sms
+
+class CustomCommandExecuted(Exception):
+    pass
 
 app = Flask(__name__)
 
@@ -11,8 +13,24 @@ tw = sms.Twilio()
 def send_message(destination, message):
     tw.send_sms(destination, message)
 
-def get_destination(message):
-    return db.find(re.findall('\s#([a-zA-Z][a-zA-Z0-9]+)', message)[-1])
+def whats_my_id(frm):
+    send_message(frm['gid'], 'your txtnonymous id is: #' + frm['tag'])
+
+def delete_my_id(frm):
+    db.delete(frm)
+    send_message(frm['gid'], 'your txtnonymous id (#' + frm['tag'] + ') has been deleted.')
+
+special_commands = {
+    'whatsmyid': whats_my_id,
+    'deletemyid': delete_my_id
+}
+
+def get_destination(message, frm):
+    hashtag = re.findall('\s#([a-zA-Z][a-zA-Z0-9]+)', message)[-1]
+    if hashtag in special_commands.keys():
+        special_commands[hashtag](frm)
+        raise CustomCommandExecuted()
+    return db.find(hashtag)
 
 def get_forwarded_message(details, frm, to):
     return details.replace(to, frm)
@@ -25,9 +43,11 @@ def on_message_recieved(from_gid, message):
     print "Message from ", from_gid, " message: ", message
     frm = db.find_or_create(from_gid)
     try:
-        to = get_destination(message)
+        to = get_destination(message, frm)
     except IndexError, Exception:
         send_message(frm['gid'], 'no destination: use a hastag like #secondfriend')
+        return
+    except CustomCommandExecuted:
         return
     send_message(to['gid'], get_forwarded_message(details, frm['tag'], to['tag']))
     update_timestamps([frm['tag'], to['tag']])
