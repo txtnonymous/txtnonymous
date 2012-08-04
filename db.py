23278c1217c3b8fc48c353820b3c88fbc4730807
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-08-04 16:30:05 +0100

    Add db function stubs

--- db.py
+++ db.py
@@ -1,1 +1,16 @@
 """Database module"""
+
+from pymongo import Connection
+connection = Connection()
+
+# Get your DB
+db = connection['default']
+
+def find_or_create(gid):
+    return None
+
+def find(gid):
+    return None
+
+def extend_timestamp(user):
+    pass
